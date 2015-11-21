5a3747738553cfdf45d658eb1d6316775f0b868f
standy <mostovoyav@gmail.com>
2015-11-21 16:57:53 +0300

    Allow string type for _id key

--- fields/types/relationship/RelationshipType.js
+++ fields/types/relationship/RelationshipType.js
@@ -13,7 +13,7 @@ function relationship(list, path, options) {
 	this.many = (options.many) ? true : false;
 	this.filters = options.filters;
 	this._defaultSize = 'full';
-	this._nativeType = keystone.mongoose.Schema.Types.ObjectId;
+	this._nativeType = options.nativeType || keystone.mongoose.Schema.Types.ObjectId;
 	this._underscoreMethods = ['format'];
 	this._properties = ['isValid', 'many', 'filters'];
 	relationship.super_.call(this, list, path, options);
